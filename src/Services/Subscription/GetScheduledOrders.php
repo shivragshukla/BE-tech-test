583ecbbeb5ee4f8737f2e45eafff446acc225ba6
Shivrag <shivragshukla001@gmail.com>
2020-10-28 07:20:20 +0530

    Get Scheduled Orders

--- src/Services/Subscription/GetScheduledOrders.php
+++ src/Services/Subscription/GetScheduledOrders.php
@@ -3,6 +3,7 @@
 namespace App\Services\Subscription;
 
 use App\Entities\Subscription;
+use App\Entities\ScheduledOrder;
 
 class GetScheduledOrders
 {
@@ -16,6 +17,21 @@ class GetScheduledOrders
      */
     public function handle(Subscription $subscription, $forNumberOfWeeks = 6)
     {
-        //
+        $scheduledOrders = [];
+        if ($subscription->getStatus() == "Cancelled") {
+            return $scheduledOrders;
+        }
+
+        for ($i = 0; $i < $forNumberOfWeeks; $i++) {
+            $nextDeliveryDate = (new \Carbon\Carbon($subscription->getNextDeliveryDate()))->startOfDay();
+            if ($subscription->getPlan() == "Fortnightly") {
+                $scheduledOrder = new ScheduledOrder($nextDeliveryDate->addWeek($i), $i%2 == 0);
+            } else {
+                $scheduledOrder = new ScheduledOrder($nextDeliveryDate->addWeek($i), true);
+            }
+            $scheduledOrders[] = $scheduledOrder;
+        }
+
+        return $scheduledOrders;
     }
 }
